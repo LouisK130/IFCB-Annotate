1058d45dc9a85c2424b5ee4d956679f100673428
Joe Futrelle <joefutrelle@gmail.com>
2017-09-08 19:04:11 +0000

    this is now the root url conf. fixed #2

--- python/env/manualclassify/classify/urls.py
+++ python/env/manualclassify/classify/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib import admin
+
 from classify import views
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.HomePageView.as_view()),
     url(r'^classify/$', views.ClassifyPageView.as_view()),
     url(r'^submitupdates/$', views.SubmitUpdatesPageView.as_view()),
