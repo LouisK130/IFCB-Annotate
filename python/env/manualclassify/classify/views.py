c402a1f9997344e9ce8516749885527600d356c6
Louis Kilfoyle <louis.kilfoyle@gmail.com>
2018-09-28 17:26:26 -0400

    Added version display

--- python/env/manualclassify/classify/views.py
+++ python/env/manualclassify/classify/views.py
@@ -18,12 +18,17 @@ logger = logging.getLogger(__name__)
 
 BATCH_SIZE = 10
 
+try:
+    from .version import VERSION
+except:
+    VERSION='unknown'
+
 def alertResponse(msg, red=True):
     return HttpResponse(json.dumps({
         'red' : red,
         'message' : msg
     }), content_type="application/json")
-    
+
 def redirectResponse(to):
     return HttpResponse(json.dumps({
         'redirect' : to
@@ -36,6 +41,7 @@ class HomePageView(TemplateView):
             return redirect(settings.LOGIN_URL)
         database.loadAllTimeseries()
         return render(request, 'index.html', {
+            'version' : VERSION,
             'failed' : request.session.pop('failed', ''),
             'username' : request.user.username,
             'timeseries_list' : database.timeseries_ids.keys(),
@@ -43,10 +49,12 @@ class HomePageView(TemplateView):
             'tag_labels' : database.getTagList(),
             'bins' : request.session.pop('bins', []),
         })
-        
+
 class LoginPageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'login.html', {})
+        return render(request, 'login.html', {
+            'version' : VERSION
+        })
     def post(self, request, **kwargs):
         if request.method == 'POST':
             username = request.POST.get('username')
@@ -69,7 +77,7 @@ def validate_email(email):
 
 def validate_password(password):
     return len(password) >= 8
-    
+
 class ValidateBinsView(TemplateView):
     def post(self, request, **kwargs):
         ts = request.POST.get('timeseries')
@@ -89,7 +97,7 @@ class ValidateBinsView(TemplateView):
             'good' : good,
             'bad' : bad
         }), content_type="application/json")
-        
+
 class SearchBinsView(TemplateView):
     def post(self, request, **kwargs):
         ts = request.POST.get('timeseries')
@@ -109,7 +117,7 @@ class SearchBinsView(TemplateView):
         return HttpResponse(json.dumps({
             'bins' : bins
         }), content_type="application/json")
-        
+
 
 class RegisterPageView(TemplateView):
     def get(self, request, **kwargs):
@@ -147,10 +155,10 @@ class LogoutPageView(TemplateView):
 
 class ClassifyPageView(TemplateView):
     def post(self, request, **kwargs):
-        
+
         if not request.user.is_authenticated:
             return redirect(settings.LOGIN_URL)
-            
+
         binsInput = request.POST.get('bins', '')
         bins = re.split(',', binsInput)
         shouldImport = json.loads(request.POST.get('import', 'false')) # boolean conversion with json lib
@@ -159,7 +167,7 @@ class ClassifyPageView(TemplateView):
         index = int(request.POST.get('index', 0))
         views = json.loads(request.POST.get('views', '[]'))
         sortby = request.POST.get('sortby', 'power')
-        
+
         if index == 0:
             for range in ranges:
                 start = range[0]
@@ -170,33 +178,33 @@ class ClassifyPageView(TemplateView):
                     bins = utils.getBinsInRange(start, end, ts)
                 else:
                     bins = utils.removeDuplicates(bins + utils.getBinsInRange(start, end, ts))
-            if len(views) > 0:   
+            if len(views) > 0:
                 bins = database.filterBins(bins, views, sortby)
-            
+
         if len(bins) == 0 or (len(bins) == 1 and bins[0] == ''):
             request.session['failed'] = 'None of those bins had results in those views.'
             return redirect('/')
-        
+
         classList = database.getClassificationList()
         tagList = database.getTagList()
-        
+
         current_bins = bins[index:index+BATCH_SIZE]
-        
+
         targets = utils.getTargets(current_bins, ts)
-        
+
         if not targets:
             request.session['bins'] = bins
             request.session['failed'] = 'One or more of the chosen bins was invalid for the chosen timeseries.'
             return redirect('/')
 
         classifications = database.getAllDataForBins(current_bins, targets, ts)
-        
+
         logger.info('{} total targets found'.format(len(targets)))
-        
+
         if shouldImport:
             logger.info('including auto results')
             classifications = database.addClassifierData(current_bins, classList, tagList, classifications, ts)
-        
+
         JS_values = {
             'timeseries' : ts,
             'classification_labels' : classList,
@@ -210,7 +218,7 @@ class ClassifyPageView(TemplateView):
             'sortby' : sortby,
             'views' : json.dumps(views)
         }
-            
+
         return render(request, 'classify.html', JS_values)
 
 class SubmitUpdatesPageView(TemplateView):
@@ -231,7 +239,7 @@ class SubmitUpdatesPageView(TemplateView):
             result.update(result2)
             result.update(result3)
             return HttpResponse(json.dumps(result))
-            
+
 class ZipDownloadPageView(TemplateView):
     def post(self, request, **kwargs):
         if request.method == 'POST':
